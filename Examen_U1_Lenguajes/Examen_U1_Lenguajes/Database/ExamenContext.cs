1a8a645a6913265724dd20b19a6bee92a156df46
Pineda04 <carlosovidiopineda8@gmail.com>
2024-10-17 23:14:44 -0600

    CreateDatabase

---
+++ Examen_U1_Lenguajes/Examen_U1_Lenguajes/Database/ExamenContext.cs
@@ -0,0 +1,49 @@
+using Examen_U1_Lenguajes.Database.Entities;
+using Examen_U1_Lenguajes.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Examen_U1_Lenguajes.Database
+{
+    public class ExamenContext : DbContext
+    {
+        private readonly IAuthService _authService;
+
+        public ExamenContext(
+            DbContextOptions options,
+            IAuthService authService
+        ) : base(options)
+        {
+            this._authService = authService;
+        }
+
+        public override Task<int> SaveChangesAsync(
+            CancellationToken cancellationToken = default)
+        {
+            var entries = ChangeTracker
+                .Entries()
+                .Where(e => e.Entity is BaseEntity && (
+                    e.State == EntityState.Added || 
+                    e.State == EntityState.Modified
+                ));
+
+            foreach (var entry in entries){
+                var entity = entry.Entity as BaseEntity;
+
+                if(entity != null){
+                    if(entry.State == EntityState.Added){
+                        entity.CreatedBy = _authService.GetUserId();
+                        entity.CreatedDate = DateTime.Now;
+                    }
+                    else {
+                        entity.UpdatedBy = _authService.GetUserId();
+                        entity.UpdatedDate = DateTime.Now;
+                    }
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public DbSet<JobTitleEntity> JobTitles { get; set; }
+    }
+}
---
+++ Examen_U1_Lenguajes/Examen_U1_Lenguajes/Services/AuthService.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Examen_U1_Lenguajes.Services.Interfaces;
+
+namespace Examen_U1_Lenguajes.Services
+{
+    public class AuthService : IAuthService
+    {
+        public string GetUserId()
+        {
+            return "1dcd6bfc-67b1-4a56-a743-841863cb0a1f";
+        }
+    }
+}
